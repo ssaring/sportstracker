e92443cc6883055ad48410ccd3e2b36db66174dc
Stefan Saring <projects@saring.de>
2013-09-27 22:07:08 +0200

    Use of Lambdas in class IdObjectList.

--- st-util/src/main/java/de/saring/util/data/IdObjectList.java
+++ st-util/src/main/java/de/saring/util/data/IdObjectList.java
@@ -1,9 +1,11 @@
 package de.saring.util.data;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * This list contains unique instances of IdObject subclasses. It will never
@@ -21,97 +23,94 @@ import java.util.List;
 public class IdObjectList<T extends IdObject> implements Iterable<T> {
 
     /** Generic list of subclasses of IdObject. */
-    private final List<T> lIdObjects = new ArrayList<> ();
+    private final List<T> lIdObjects = new ArrayList<>();
 
     /** List of listeners which will be notified on each list content change. */
-    private final List<IdObjectListChangeListener> listChangelisteners = new ArrayList<> ();
+    private final List<IdObjectListChangeListener> listChangelisteners = new ArrayList<>();
 
     /**
      * Returns the IdObject with the specified ID.
+     * 
      * @param id ID of IdObject
      * @return the IdObject object or null
      */
-    public T getByID (int id) {
-        for (T t : this.lIdObjects) {
-            if (t.getId () == id) {
-                return t;
-            }
-        }
-        return null;
+    public T getByID(int id) {
+        Optional<T> oIdObject = lIdObjects.stream().filter(o -> o.getId() == id).findFirst();
+        return oIdObject.orElse(null);
     }
     
     /**
      * Returns the IdObject at the specified index. Throws an
      * IndexOutOfBoundsException when the index is not valid.
+     *
      * @param index the index of the IdObject
      * @return the IdObject
      */
-    public T getAt (int index) {
-        return lIdObjects.get (index);
+    public T getAt(int index) {
+        return lIdObjects.get(index);
     }
-    
+
     /**
      * Returns the index of the specified object in the list or -1 if it is not
      * contained.
+     *
      * @param t the object to lookup in the list
      * @return the index of the object or -1
      */
-    public int indexOf (T t) {
-        return lIdObjects.indexOf (t);
+    public int indexOf(T t) {
+        return lIdObjects.indexOf(t);
     }
     
     /**
      * Checks whether the specified object is contained in list.
+     *
      * @param t the object to lookup in the list
-     * @return true if the list contains the specified object 
+     * @return true if the list contains the specified object
      */
     public boolean contains(T t) {
         return lIdObjects.contains(t);
     }
-    
+
     /**
      * Stores the specified IdObject in the list. If there is allready an
-     * IDObject with that ID then the old object will be overwritten.
-     * Otherwise the new one will be added to the end of the list.
+     * IDObject with that ID then the old object will be overwritten. Otherwise
+     * the new one will be added to the end of the list.
+     *
      * @param t the IdObject to store (must not be null)
      */
-    public void set (T t) {
+    public void set(T t) {
         if (t == null) {
-            throw new IllegalArgumentException ("Must not be null!");
+            throw new IllegalArgumentException("Must not be null!");
         }
-        if (t.getId () <= 0) {
-            throw new IllegalArgumentException ("ID must be a positive integer > 0!");
+        if (t.getId() <= 0) {
+            throw new IllegalArgumentException("ID must be a positive integer > 0!");
         }
 
         try {
-            // replace old IdObject if there is one with the ID of the new one
-            for (int i = 0; i < this.lIdObjects.size (); i++) {
-                T temp = this.lIdObjects.get (i);
-                if (t.getId () == temp.getId ()) {
-                    this.lIdObjects.set (i, t);
-                    return;
-                }
+            int index = lIdObjects.indexOf(t);
+            if (index >= 0) {
+                // replace old IdObject if there is one with the ID of the new one
+                this.lIdObjects.set(index, t);
+            } else {
+                // the object has a new ID => add to end of list
+                this.lIdObjects.add(t);
             }
-
-            // the object has a new ID => add to end of list
-            this.lIdObjects.add (t);
-        }
-        finally {
+        } finally {
             notifyAllListChangelisteners(t);
         }
     }
-    
-        
+
     /**
      * Removes the IdObject with the specified ID from the list.
+     *
      * @param id ID of IDObject to remove
      * @return true on success
      */
-    public boolean removeByID (int id) {
-        
-        T t = getByID (id);
+    public boolean removeByID(int id) {
+
+        T t = getByID(id);
         if (t != null) {
-            boolean removed = this.lIdObjects.remove (t);
+            boolean removed = this.lIdObjects.remove(t);
             if (removed) {
                 notifyAllListChangelisteners(null);
             }
@@ -119,80 +118,84 @@ public class IdObjectList<T extends IdObject> implements Iterable<T> {
         }
         return false;
     }
-    
-    /** 
-     * This method returns an unique ID, which is not in use yet. 
+
+    /**
+     * This method returns an unique ID, which is not in use yet.
+     *
      * @return a new unused ID
      */
-    public int getNewID () {
-        
-        HashSet<Integer> hsIDs = new HashSet<> ();
-        for (T t : this.lIdObjects) {
-            hsIDs.add (t.getId ());
-        }
+    public int getNewID() {
+        Set<Integer> hsIDs = lIdObjects.stream()
+                .map(o -> o.getId())
+                .collect(Collectors.toSet());
 
         // find first unused ID
         int newID = 1;
-        while (hsIDs.contains (newID)) {
+        while (hsIDs.contains(newID)) {
             newID++;
         }
         return newID;
     }
-    
+
     /**
      * Returns the size of the list.
+     *
      * @return the size of the list
      */
-    public int size () {
-        return lIdObjects.size ();
+    public int size() {
+        return lIdObjects.size();
     }
-    
+
     /**
      * Returns an interator over the list elements in proper sequence.
+     *
      * @return interator over the list elements
      */
     @Override
-    public Iterator<T> iterator () {
-        return lIdObjects.iterator ();
+    public Iterator<T> iterator() {
+        return lIdObjects.iterator();
     }
 
     /**
      * Returns the internal list of IdObject. Only subclasses can directly
      * access this list.
+     *
      * @return the internal list of IdObject
      */
-    protected List<T> getIDObjects () {
+    protected List<T> getIDObjects() {
         return lIdObjects;
-    } 
+    }
 
     /**
      * Adds the specified IdObjectListChangeListener to the list of listeners
      * which will be notified on each list change.
+     *
      * @param listener the IdObjectListChangeListener to add
      */
-    public void addListChangeListener (IdObjectListChangeListener listener) {
-        listChangelisteners.add (listener);
+    public void addListChangeListener(IdObjectListChangeListener listener) {
+        listChangelisteners.add(listener);
     }
 
     /**
      * Notifies all registered listeners that the content of the list has been
      * changed.
-     * 
+     *
      * @param changedObject the added / changed object (or null when removed)
      */
     protected void notifyAllListChangelisteners(IdObject changedObject) {
         listChangelisteners.forEach(listener -> listener.listChanged(changedObject));
     }
 
-    /** 
-     * Returns a string representation of this object. 
+    /**
+     * Returns a string representation of this object.
+     *
      * @return string with object content
      */
     @Override
-    public String toString() {        
+    public String toString() {
         StringBuilder sBuilder = new StringBuilder();
         sBuilder.append(this.getClass().getName()).append(":\n");
         lIdObjects.forEach(entry -> sBuilder.append(entry));
-        return sBuilder.toString ();
+        return sBuilder.toString();
     }
 }
