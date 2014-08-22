de1a2d31b9682db73d9cca5143351662ccced878
Stefan Saring <projects@saring.de>
2014-08-22 16:24:43 +0200

    * The JavaFX dialog is now always placed in center of the Swing JFrame.

--- sportstracker/src/main/java/de/saring/sportstracker/gui/STControllerImpl.java
+++ sportstracker/src/main/java/de/saring/sportstracker/gui/STControllerImpl.java
@@ -12,6 +12,7 @@ import de.saring.sportstracker.gui.views.EntryView;
 import de.saring.util.data.IdDateObject;
 import de.saring.util.data.IdDateObjectList;
 import javafx.application.Platform;
+import javafx.stage.Stage;
 import org.jdesktop.application.Action;
 import org.jdesktop.application.Task;
 
@@ -19,6 +20,7 @@ import javax.inject.Inject;
 import javax.inject.Provider;
 import javax.inject.Singleton;
 import javax.swing.*;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.io.File;
 import java.time.LocalDate;
@@ -578,9 +580,24 @@ public class STControllerImpl implements STController {
     public void showAboutDialog() {
         // TODO remove workarounds after migration of main window to JavaFX
         // JavaFX UI code needs to be executed on the JavaFX application thread.
-        // Bring dummy JavaFX window to front, is always behind the Swing window.
         Platform.runLater(() -> {
-            context.getPrimaryStage().toFront();
+            // bring dummy JavaFX window to front, is always behind the Swing window
+            // context.getPrimaryStage().toFront();
+
+            // TODO move the placement and modal functionality to an external method
+
+            // place the transparent dummy JavaFX main window in the center of the Swing window
+            // => the JavaFX dialog uses the dummy window as parent, so it's placed in the center of the Swing window
+            final Rectangle swingFrameBounds = context.getMainFrame().getBounds();
+            final Stage fxStage = context.getPrimaryStage();
+            fxStage.setX(swingFrameBounds.getCenterX());
+            fxStage.setY(swingFrameBounds.getCenterY());
+
+            // problem: the new Stage position is not being used for the dialog
+            // workaround: hide and show the dummy stage (is invisible anyway), then it works
+            fxStage.hide();
+            fxStage.show();
+
             prAboutDialogController.get().show(context.getPrimaryStage());
         });
     }
