6dcc7314805ca36339d36c90ff65c600a7b51fb7
Stefan Saring <projects@saring.de>
2014-09-12 17:18:14 +0200

    * NoteDialog: added new ViewModel class NoteModel. * NoteDialog: use of JavaFX binding between UI controls and ViewModel.

--- sportstracker/src/main/java/de/saring/sportstracker/gui/dialogsfx/NoteDialogController.java
+++ sportstracker/src/main/java/de/saring/sportstracker/gui/dialogsfx/NoteDialogController.java
@@ -5,15 +5,24 @@ import de.saring.sportstracker.gui.STContext;
 import de.saring.sportstracker.gui.STDocument;
 import de.saring.sportstracker.gui.util.InputValidators;
 import de.saring.util.gui.javafx.GuiceFxmlLoader;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Window;
+import javafx.util.converter.NumberStringConverter;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 /**
  * Controller (MVC) class of the Note dialog for editing / adding Note entries.
@@ -39,8 +48,8 @@ public class NoteDialogController extends AbstractDialogController {
     @FXML
     private TextArea taText;
 
-    /** This is the Note object edited in this dialog. */
-    private Note note;
+    /** Model (MVC) of the edited Note. */
+    private NoteModel noteModel;
 
     /**
      * Standard c'tor for dependency injection.
@@ -62,7 +71,7 @@ public class NoteDialogController extends AbstractDialogController {
      * @param note Note to be edited
      */
     public void show(final Window parent, final Note note) {
-        this.note = note;
+        this.noteModel = new NoteModel(note);
 
         final String dlgTitleKey = note.getText() == null ? "st.dlg.note.title.add" : "st.dlg.note.title";
         final String dlgTitle = context.getFxResources().getString(dlgTitleKey);
@@ -70,25 +79,23 @@ public class NoteDialogController extends AbstractDialogController {
         showEditDialog("/fxml/NoteDialog.fxml", parent, dlgTitle);
     }
 
+    // TODO rename method???
     @Override
     protected void setInitialValues() {
 
-        // TODO use JavaFX Binding?
-        dpDate.setValue(note.getDateTime().toLocalDate());
-        tfHour.setText(String.format("%02d", note.getDateTime().getHour()));
-        tfMinute.setText(String.format("%02d", note.getDateTime().getMinute()));
-        taText.setText(note.getText());
+        dpDate.valueProperty().bindBidirectional(noteModel.date);
+        tfHour.textProperty().bindBidirectional(noteModel.hour, new NumberStringConverter("00"));
+        tfMinute.textProperty().bindBidirectional(noteModel.minute, new NumberStringConverter("00"));
+        taText.textProperty().bindBidirectional(noteModel.text);
     }
 
     @Override
     protected boolean validateAndStore() {
 
-        // create a new Note, because user can cancel after validation errors
-        // => so we don't modify the original Note
-        Note newNote = new Note(note.getId());
+        // TODO use frameworks such as JideFX or ControlsFX for validation
 
         // check date input
-        if (dpDate.getValue() == null) {
+        if (noteModel.date.getValue() == null) {
             context.showFxErrorDialog(dpDate.getScene().getWindow(),
                     "common.error", "st.dlg.note.error.date");
             dpDate.requestFocus();
@@ -96,32 +103,85 @@ public class NoteDialogController extends AbstractDialogController {
         }
 
         // check time inputs
-        Integer hour = InputValidators.getRequiredTextControlIntegerValue(context, tfHour, 0, 23,
-                "common.error", "st.dlg.note.error.time");
-        if (hour == null) {
+        if (InputValidators.getRequiredTextControlIntegerValue(context, tfHour, 0, 23,
+                "common.error", "st.dlg.note.error.time") == null) {
             return false;
         }
 
-        Integer minute = InputValidators.getRequiredTextControlIntegerValue(context, tfMinute, 0, 59,
-                "common.error", "st.dlg.note.error.time");
-        if (minute == null) {
+        if (InputValidators.getRequiredTextControlIntegerValue(context, tfMinute, 0, 59,
+                "common.error", "st.dlg.note.error.time") == null) {
             return false;
         }
-
-        // store date and time of the note
-        LocalDateTime newDateTime = dpDate.getValue().atTime(hour, minute);
-        newNote.setDateTime(newDateTime);
 
         // get note text
-        String strText = InputValidators.getRequiredTextControlValue(context, taText,
-                "common.error", "st.dlg.note.error.no_text");
-        if (strText == null) {
+        if (InputValidators.getRequiredTextControlValue(context, taText,
+                "common.error", "st.dlg.note.error.no_text") == null) {
             return false;
         }
-        newNote.setText(strText);
 
         // finally store the new Note
+        final Note newNote = noteModel.getNote();
         document.getNoteList().set(newNote);
         return true;
+    }
+
+    // TODO move class to a separate file?
+
+    /**
+     * This ViewModel class provides JavaFX properties of all Note attributes to be edited in the dialog.
+     * So they can be bound to the appropriate dialog view controls.
+     */
+    private static final class NoteModel {
+
+        private final int id;
+        private final ObjectProperty<LocalDate> date;
+        private final IntegerProperty hour;
+        private final IntegerProperty minute;
+        private final StringProperty text;
+
+        /**
+         * Creates the NoteModel with JavaFX properties for the passed Note object.
+         *
+         * @param note Note to be edited
+         */
+        public NoteModel(final Note note) {
+            this.id = note.getId();
+            this.date = new SimpleObjectProperty(note.getDateTime().toLocalDate());
+            this.hour = new SimpleIntegerProperty(note.getDateTime().getHour());
+            this.minute = new SimpleIntegerProperty(note.getDateTime().getMinute());
+            this.text = new SimpleStringProperty(note.getText());
+
+            // define validators which check every value change
+            // TODO can't be done this way => use frameworks such as JideFX or ControlsFX for validation!
+//            this.date.addListener((observable, oldValue, newValue) -> {
+//                if (newValue == null) {
+//                    this.date.set(oldValue);
+//                }
+//            });
+//
+//            this.hour.addListener((observable, oldValue, newValue) -> {
+//                if (newValue == null || newValue.intValue() < 0 || newValue.intValue() > 23) {
+//                    this.hour.set(oldValue.intValue());
+//                }
+//            });
+//
+//            this.minute.addListener((observable, oldValue, newValue) -> {
+//                if (newValue == null || newValue.intValue() < 0 || newValue.intValue() > 59) {
+//                    this.minute.set(oldValue.intValue());
+//                }
+//            });
+        }
+
+        /**
+         * Creates a new Note domain object from the edited JavaFX properties.
+         *
+         * @return Note
+         */
+        public Note getNote() {
+            final Note note = new Note(id);
+            note.setDateTime(LocalDateTime.of(date.get(), LocalTime.of(hour.getValue(), minute.getValue())));
+            note.setText(text.getValue().trim());
+            return note;
+        }
     }
 }
