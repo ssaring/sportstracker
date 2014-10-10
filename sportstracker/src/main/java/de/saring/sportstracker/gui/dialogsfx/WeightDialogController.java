a05e058d92c78ff6878fec01c172a605124b9cbe
Stefan Saring <projects@saring.de>
2014-10-10 13:59:57 +0200

    * Extracted WeightViewModel to a separate file.

--- sportstracker/src/main/java/de/saring/sportstracker/gui/dialogsfx/WeightDialogController.java
+++ sportstracker/src/main/java/de/saring/sportstracker/gui/dialogsfx/WeightDialogController.java
@@ -3,19 +3,8 @@ package de.saring.sportstracker.gui.dialogsfx;
 import de.saring.sportstracker.data.Weight;
 import de.saring.sportstracker.gui.STContext;
 import de.saring.sportstracker.gui.STDocument;
-import de.saring.util.StringUtils;
 import de.saring.util.ValidationUtils;
 import de.saring.util.gui.javafx.GuiceFxmlLoader;
-import de.saring.util.unitcalc.ConvertUtils;
-import de.saring.util.unitcalc.FormatUtils.UnitSystem;
-import javafx.beans.property.FloatProperty;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleFloatProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Control;
 import javafx.scene.control.DatePicker;
@@ -29,9 +18,6 @@ import org.controlsfx.validation.Validator;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 /**
  * Controller (MVC) class of the Wieght dialog for editing / adding Weight entries.
@@ -128,61 +114,5 @@ public class WeightDialogController extends AbstractDialogController {
         final Weight newWeight = weightViewModel.getWeight();
         document.getWeightList().set(newWeight);
         return true;
-    }
-
-    /**
-     * This ViewModel class provides JavaFX properties of all Weight attributes to be edited in the dialog.
-     * So they can be bound to the appropriate dialog view controls.
-     */
-    private static final class WeightViewModel {
-
-        private final int id;
-        private final UnitSystem unitSystem;
-
-        private final ObjectProperty<LocalDate> date;
-        private final IntegerProperty hour;
-        private final IntegerProperty minute;
-        private final FloatProperty value;
-        private final StringProperty comment;
-
-        /**
-         * Creates the WeightViewModel with JavaFX properties for the passed Weight object.
-         *
-         * @param weight Weight to be edited
-         * @param unitSystem the unit system currently used in the UI
-         */
-        public WeightViewModel(final Weight weight, final UnitSystem unitSystem) {
-            this.id = weight.getId();
-            this.date = new SimpleObjectProperty(weight.getDateTime().toLocalDate());
-            this.hour = new SimpleIntegerProperty(weight.getDateTime().getHour());
-            this.minute = new SimpleIntegerProperty(weight.getDateTime().getMinute());
-            this.value = new SimpleFloatProperty(weight.getValue());
-            this.comment = new SimpleStringProperty(StringUtils.getTextOrEmptyString(weight.getComment()));
-
-            // convert weight value when english unit system is enabled
-            this.unitSystem = unitSystem;
-            if (unitSystem == UnitSystem.English) {
-                this.value.set((float) ConvertUtils.convertKilogram2Lbs(weight.getValue()));
-            }
-        }
-
-        /**
-         * Creates a new Weight domain object from the edited JavaFX properties.
-         *
-         * @return Weight
-         */
-        public Weight getWeight() {
-            final Weight weight = new Weight(id);
-            weight.setDateTime(LocalDateTime.of(date.get(), LocalTime.of(hour.getValue(), minute.getValue())));
-            weight.setValue(value.get());
-            // ignore empty text for optional inputs
-            weight.setComment(StringUtils.getTrimmedTextOrNull(comment.getValue()));
-
-            // convert weight value when english unit system is enabled
-            if (unitSystem == UnitSystem.English) {
-                weight.setValue((float) ConvertUtils.convertLbs2Kilogram(weight.getValue()));
-            }
-            return weight;
-        }
     }
 }
