a111b0437eec025a3177fdc8a285e8b04fa5e12e
Stefan Saring <projects@saring.de>
2014-10-29 14:57:37 +0100

    * SportType Dialog: added initial version of FXML layout file and SportTypeDialogController.

---
+++ sportstracker/src/main/java/de/saring/sportstracker/gui/dialogsfx/SportTypeDialogController.java
@@ -0,0 +1,120 @@
+package de.saring.sportstracker.gui.dialogsfx;
+
+import de.saring.sportstracker.data.Equipment;
+import de.saring.sportstracker.data.SportSubType;
+import de.saring.sportstracker.data.SportType;
+import de.saring.sportstracker.gui.STContext;
+import de.saring.sportstracker.gui.STDocument;
+import de.saring.util.gui.javafx.GuiceFxmlLoader;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Window;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * Controller (MVC) class of the Sport Type dialog for editing / adding SportType entries.
+ *
+ * @author Stefan Saring
+ */
+@Singleton
+public class SportTypeDialogController extends AbstractDialogController {
+
+    private final STDocument document;
+
+    @FXML
+    private TextField tfName;
+
+    @FXML
+    private CheckBox cbRecordDistance;
+
+    @FXML
+    private ColorPicker cpColor;
+
+    @FXML
+    private ListView<SportSubType> liSportSubtypes;
+
+    @FXML
+    private ListView<Equipment> liEquipments;
+
+    @FXML
+    private Button btSportSubtypeEdit;
+    @FXML
+    private Button btSportSubtypeDelete;
+
+    @FXML
+    private Button btEquipmentEdit;
+    @FXML
+    private Button btEquipmentDelete;
+
+
+    /** ViewModel of the edited SportType. */
+    // TODO private NoteViewModel noteViewModel;
+
+    /**
+     * Standard c'tor for dependency injection.
+     *
+     * @param context the SportsTracker UI context
+     * @param document the SportsTracker model/document
+     * @param guiceFxmlLoader the Guice FXML loader
+     */
+    @Inject
+    public SportTypeDialogController(final STContext context, final STDocument document,
+                                     final GuiceFxmlLoader guiceFxmlLoader) {
+        super(context, guiceFxmlLoader);
+        this.document = document;
+    }
+
+    /**
+     * Displays the Sport Type dialog for the passed SportType instance.
+     *
+     * @param parent parent window of the dialog
+     * @param sportType sport type to be edited
+     */
+    public void show(final Window parent, final SportType sportType) {
+        // TODO this.noteViewModel = new NoteViewModel(note);
+
+        final String dlgTitleKey = sportType.getName() == null ?
+                "st.dlg.sporttype.title.add" : "st.dlg.sporttype.title";
+        final String dlgTitle = context.getFxResources().getString(dlgTitleKey);
+
+        showEditDialog("/fxml/SportTypeDialog.fxml", parent, dlgTitle);
+    }
+
+    @Override
+    protected void setupDialogControls() {
+
+        /* TODO
+        // setup binding between view model and the UI controls
+        dpDate.valueProperty().bindBidirectional(noteViewModel.date);
+        tfHour.textProperty().bindBidirectional(noteViewModel.hour, new NumberStringConverter("00"));
+        tfMinute.textProperty().bindBidirectional(noteViewModel.minute, new NumberStringConverter("00"));
+        taText.textProperty().bindBidirectional(noteViewModel.text);
+
+        // setup validation of the UI controls
+        validationSupport.registerValidator(dpDate,
+                Validator.createEmptyValidator(context.getFxResources().getString("st.dlg.note.error.date")));
+        validationSupport.registerValidator(tfHour, true, (Control control, String newValue) ->
+                ValidationResult.fromErrorIf(tfHour, context.getFxResources().getString("st.dlg.note.error.time"),
+                        !ValidationUtils.isValueIntegerBetween(newValue, 0, 23)));
+        validationSupport.registerValidator(tfMinute, true, (Control control, String newValue) ->
+                ValidationResult.fromErrorIf(tfMinute, context.getFxResources().getString("st.dlg.note.error.time"),
+                        !ValidationUtils.isValueIntegerBetween(newValue, 0, 59)));
+        validationSupport.registerValidator(taText,
+                Validator.createEmptyValidator(context.getFxResources().getString("st.dlg.note.error.no_text"))); */
+    }
+
+    @Override
+    protected boolean validateAndStore() {
+
+        /* TODO store the new SportType, no further validation needed
+        final Note newNote = noteViewModel.getNote();
+        document.getNoteList().set(newNote); */
+        return true;
+    }
+}
