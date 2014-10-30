d9c86b45d8413ea435875994fd6b7b68c0c57e67
Stefan Saring <projects@saring.de>
2014-10-30 21:24:47 +0100

    * SportListType Dialog: setup of sport type list, use custom ListCell for sport types.

--- sportstracker/src/main/java/de/saring/sportstracker/gui/dialogsfx/SportTypeListDialogController.java
+++ sportstracker/src/main/java/de/saring/sportstracker/gui/dialogsfx/SportTypeListDialogController.java
@@ -4,9 +4,15 @@ import de.saring.sportstracker.data.SportType;
 import de.saring.sportstracker.gui.STContext;
 import de.saring.sportstracker.gui.STDocument;
 import de.saring.util.gui.javafx.GuiceFxmlLoader;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.paint.Color;
 import javafx.stage.Window;
 
 import javax.inject.Inject;
@@ -56,6 +62,52 @@ public class SportTypeListDialogController extends AbstractDialogController {
 
     @Override
     protected void setupDialogControls() {
-        // TODO setup binding, disable edit and delete buttons when no selection
+
+        // setup SportType list
+        liSportTypes.setCellFactory(list -> new SportTypeListCell());
+        updateSportTypeList();
+
+        // Edit and Delete buttons must be disabled when there is no selection in list
+        final BooleanBinding selected = Bindings.isNull(liSportTypes.getSelectionModel().selectedItemProperty());
+        btEdit.disableProperty().bind(selected);
+        btDelete.disableProperty().bind(selected);
+    }
+
+    private void updateSportTypeList() {
+        final ObservableList<SportType> olSportTypes = FXCollections.observableArrayList();
+        document.getSportTypeList().forEach(sportType -> olSportTypes.add(sportType));
+        liSportTypes.setItems(olSportTypes);
+    }
+
+    /**
+     * Custom ListCell implementation for displaying the sport types in the ListView by using its name and color.
+     */
+    private static class SportTypeListCell extends ListCell<SportType> {
+
+        @Override
+        protected void updateItem(final SportType item, final boolean empty) {
+            super.updateItem(item, empty);
+
+            setText(item == null ? null : item.getName());
+
+            if (item != null) {
+                setTextFill(getJavaFXColorForSportType(item));
+            }
+        }
+
+        @Override
+        public void updateSelected(final boolean selected) {
+            super.updateSelected(selected);
+
+            // use white text color for selected sport types, updateItem() is not called on selection changes
+            if (getItem() != null) {
+                setTextFill(selected ? Color.WHITE : getJavaFXColorForSportType(getItem()));
+            }
+        }
+
+        private Color getJavaFXColorForSportType(final SportType sportType) {
+            final java.awt.Color awtColor = sportType.getColor();
+            return new Color(awtColor.getRed() / 255f, awtColor.getGreen() / 255f, awtColor.getBlue() / 255f, 1);
+        }
     }
 }
