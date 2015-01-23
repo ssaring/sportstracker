1cb4d0b3e7b7a8d59f67a7b2372e31294fc17354
Stefan Saring <projects@saxsys.de>
2015-01-23 09:07:02 +0100

    ViewPrinter: extracted print functions from AbstractEntryViewController to this new class. ViewPrinter: remember the printer selection and page layout for next printing.

--- sportstracker/src/main/java/de/saring/sportstracker/gui/views/AbstractEntryViewController.java
+++ sportstracker/src/main/java/de/saring/sportstracker/gui/views/AbstractEntryViewController.java
@@ -2,17 +2,11 @@ package de.saring.sportstracker.gui.views;
 
 import java.io.IOException;
 
-import javafx.print.PageLayout;
-import javafx.print.PrinterJob;
-import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.control.Alert;
-import javafx.scene.image.ImageView;
-import javafx.scene.image.WritableImage;
-import javafx.scene.transform.Scale;
+
+import javax.inject.Inject;
 
 import de.saring.sportstracker.core.STException;
-import de.saring.sportstracker.core.STExceptionID;
 import de.saring.sportstracker.gui.STContext;
 import de.saring.sportstracker.gui.STController;
 import de.saring.sportstracker.gui.STDocument;
@@ -30,6 +24,9 @@ public abstract class AbstractEntryViewController implements EntryViewController
     private final STContext context;
     private final STDocument document;
     private final STController controller;
+
+    @Inject
+    private ViewPrinter viewPrinter;
 
     private Parent rootNode;
 
@@ -97,29 +94,7 @@ public abstract class AbstractEntryViewController implements EntryViewController
 
     @Override
     public void print() throws STException {
-        // TODO execution on a separate thread?
-
-        final PrinterJob printerJob = PrinterJob.createPrinterJob();
-        if (printerJob == null) {
-            // no printer available
-            context.showMessageDialog(context.getPrimaryStage(), Alert.AlertType.ERROR, //
-                    "common.error", "st.main.error.print_view.no_printer");
-            return;
-        }
-
-        // display print dialog for confirmation and configuration by the user
-        final boolean printConfirmed = printerJob.showPrintDialog(getContext().getPrimaryStage());
-        // TODO remember the printer configuration (printer, page layout) for next printing? Store JobSettings?
-
-        if (printConfirmed) {
-            if (printView(printerJob, rootNode)) {
-                if (!printerJob.endJob()) {
-                    throw new STException(STExceptionID.GUI_PRINT_VIEW_FAILED, "Failed to end the print job!");
-                }
-            } else {
-                throw new STException(STExceptionID.GUI_PRINT_VIEW_FAILED, "Failed to execute the view print!");
-            }
-        }
+        viewPrinter.printView(rootNode);
     }
 
     /**
@@ -159,34 +134,5 @@ public abstract class AbstractEntryViewController implements EntryViewController
      */
     protected STController getController() {
         return controller;
-    }
-
-    /**
-     * Prints the specified view node completely to one single page.
-     *
-     * @param printerJob printer job which defines the printer, page layout, ...
-     * @param view view node to print
-     * @return true when the view was printed successfully
-     */
-    private boolean printView(final PrinterJob printerJob, final Node view) {
-
-        // the view needs to be scaled to fit the selected page layout of the PrinterJob
-        // => the passed view node can't be scaled, this would scale the displayed UI
-        // => solution: create a snapshot image for printing and scale this image
-        final WritableImage snapshot = view.snapshot(null, null);
-        final ImageView ivSnapshot = new ImageView(snapshot);
-
-        // compute the needed scaling (aspect ratio must be kept)
-        final PageLayout pageLayout = printerJob.getJobSettings().getPageLayout();
-        final double scaleX = pageLayout.getPrintableWidth() / ivSnapshot.getImage().getWidth();
-        final double scaleY = pageLayout.getPrintableHeight() / ivSnapshot.getImage().getHeight();
-        final double scale = Math.min(scaleX, scaleY);
-
-        // scale the calendar image only when it's too big for the selected page
-        if (scale < 1.0) {
-            ivSnapshot.getTransforms().add(new Scale(scale, scale));
-        }
-
-        return printerJob.printPage(ivSnapshot);
     }
 }
