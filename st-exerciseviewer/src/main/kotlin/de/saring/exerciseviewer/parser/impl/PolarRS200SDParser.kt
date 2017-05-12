ff1ab9b04cb1dae237311f378f63ea45b35d8e93
Stefan Saring <projects@saring.de>
2017-05-12 16:11:51 +0200

    ExerciseViewer: implemented PolarRS200SDParser and unit tests in Kotlin

---
+++ st-exerciseviewer/src/main/kotlin/de/saring/exerciseviewer/parser/impl/PolarRS200SDParser.kt
@@ -0,0 +1,138 @@
+package de.saring.exerciseviewer.parser.impl
+
+import de.saring.exerciseviewer.core.EVException
+import de.saring.exerciseviewer.data.*
+import de.saring.exerciseviewer.parser.AbstractExerciseParser
+import de.saring.exerciseviewer.parser.ExerciseParserInfo
+import org.jdom2.Element
+import org.jdom2.input.SAXBuilder
+import java.time.LocalDateTime
+
+/**
+ * This implementation of an ExerciseParser is for reading the XML-based exercise files of the Polar RS200SD devices.
+ *
+ * @author Stefan Saring (the C# version was done by Jacob Ilsoe Christensen)
+ */
+class PolarRS200SDParser : AbstractExerciseParser() {
+
+    /** Information about this parser. */
+    private val info = ExerciseParserInfo("Polar RS200", arrayOf("xml", "XML"))
+
+    override
+    fun getInfo(): ExerciseParserInfo = info
+
+    override
+    fun parseExercise(filename: String): EVExercise {
+
+        try {
+            val document = SAXBuilder().build(filename)
+            return parseExerciseElement(document.rootElement)
+        }
+        catch (e: Exception) {
+            throw EVException("Failed to read the RS200SD exercise file '${filename}'!", e)
+        }
+    }
+
+    /**
+     * Parses the exercise data from the specified rs200_session element.
+     */
+    private fun parseExerciseElement(eSession: Element): EVExercise {
+
+        // parse basic exercise data
+        val exercise = EVExercise()
+        exercise.fileType = EVExercise.ExerciseFileType.RS200SDRAW
+        exercise.deviceName = "Polar RS200"
+        exercise.recordingMode = RecordingMode()
+        exercise.sampleList = arrayOf()
+
+        val eSessionData = eSession.getChild("session_data")
+        exercise.dateTime = LocalDateTime.of(
+                eSessionData.getChildText("year").toInt(),
+                eSessionData.getChildText("month").toInt(),
+                eSessionData.getChildText("day").toInt(),
+                eSessionData.getChildText("start_hour").toInt(),
+                eSessionData.getChildText("start_minute").toInt(),
+                eSessionData.getChildText("start_second").toInt())
+
+        val eSummary = eSessionData.getChild("summary")
+        exercise.duration = (eSummary.getChildText("length").toDouble() * 10.0).toInt()
+        exercise.heartRateAVG = eSummary.getChildText("avg_hr").toShort()
+        exercise.heartRateMax = eSummary.getChildText("max_hr").toShort()
+        exercise.energy = eSummary.getChildText("calories").toInt()
+        val maxSetHr = eSummary.getChildText("max_set_hr").toInt()
+
+        // parse heartrate limits
+        val eSortzoneList = eSessionData.getChild("sportzones").getChildren("sportzone")
+        val heartRateLimits = mutableListOf<HeartRateLimit>()
+
+        for (eSortzone in eSortzoneList) {
+            val heartRateLimit = HeartRateLimit()
+            heartRateLimits.add(heartRateLimit)
+
+            heartRateLimit.lowerHeartRate = (eSortzone.getChildText("low_percent").toInt() * maxSetHr / 100).toShort()
+            heartRateLimit.upperHeartRate = (eSortzone.getChildText("high_percent").toInt() * maxSetHr / 100).toShort()
+            heartRateLimit.timeWithin = eSortzone.getChildText("time_on").toDouble().toInt()
+        }
+        exercise.heartRateLimits = heartRateLimits.toTypedArray()
+
+        // parse speed data when available
+        val hasSpeedData = eSessionData.getChildText("has_pace_data").toBoolean()
+        val distance = eSummary.getChildText("total_distance").toInt()
+
+        // "has_pace_data" can also be true, when no speed data was recorded and "total_distance" is 0
+        // => then the speed needs to be disabled in exercise, ExerciseViewer will have problems to display
+        //    the inconsistent data (see SourceForge bug #1524834)
+        if (hasSpeedData && distance > 0) {
+            exercise.speed = ExerciseSpeed()
+            exercise.speed.distance = distance
+            exercise.speed.speedMax = convertSpeed(eSummary.getChildText("max_pace").toFloat())
+            exercise.speed.speedAVG = convertSpeed(eSummary.getChildText("avg_pace").toFloat())
+            exercise.recordingMode.isSpeed = true
+        } else {
+            exercise.recordingMode.isSpeed = false
+        }
+
+        // parse laps (they are in reverse order in XML)
+        val eLapList = eSessionData.getChild("laps").getChildren("lap")
+        val laps = mutableListOf<Lap>()
+
+        for (eLap in eLapList) {
+            val lap = Lap()
+            laps.add(lap)
+
+            lap.timeSplit = (eLap.getChildText("lap_end_time").toDouble() * 10.0).toInt()
+            lap.heartRateSplit = eLap.getChildText("end_hr").toShort()
+            lap.heartRateAVG = eLap.getChildText("avg_hr").toShort()
+            lap.heartRateMax = eLap.getChildText("max_hr").toShort()
+
+            if (hasSpeedData) {
+                lap.speed = LapSpeed()
+                lap.speed.distance = eLap.getChildText("lap_length").toInt()
+                lap.speed.speedEnd = convertSpeed(eLap.getChildText("end_pace").toFloat())
+                lap.speed.speedAVG = convertSpeed(eLap.getChildText("avg_pace").toFloat())
+            }
+        }
+
+        laps.reverse()
+        exercise.lapList = laps.toTypedArray()
+
+        // calculate distance for all laps
+        if (hasSpeedData) {
+            var accumulatedSpeed = 0
+
+            for (lap in exercise.lapList) {
+                lap.speed.distance += accumulatedSpeed
+                accumulatedSpeed = lap.speed.distance
+            }
+        }
+
+        return exercise
+    }
+
+    /**
+     * Helper method for converting parsed speed to km/h.
+     */
+    private fun convertSpeed(speed: Float): Float {
+        return if (speed > 0) 3600 / speed else 0f
+    }
+}
