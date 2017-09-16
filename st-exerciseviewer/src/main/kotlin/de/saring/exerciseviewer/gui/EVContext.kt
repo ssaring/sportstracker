015a491c77df58e064d4ed2a4bdf861335ff8883
Stefan Saring <projects@saring.de>
2017-09-16 13:52:31 +0200

    EV: ported EVContext to Kotlin.

--- st-exerciseviewer/src/main/kotlin/de/saring/exerciseviewer/gui/EVContext.kt
+++ st-exerciseviewer/src/main/kotlin/de/saring/exerciseviewer/gui/EVContext.kt
@@ -1,13 +1,13 @@
-package de.saring.exerciseviewer.gui;
+package de.saring.exerciseviewer.gui
 
-import de.saring.util.AppResources;
-import de.saring.util.unitcalc.FormatUtils;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.stage.Stage;
-import javafx.stage.Window;
+import de.saring.util.AppResources
+import de.saring.util.unitcalc.FormatUtils
+import javafx.scene.control.Alert
+import javafx.scene.control.ButtonType
+import javafx.stage.Stage
+import javafx.stage.Window
 
-import java.util.Optional;
+import java.util.Optional
 
 /**
  * This is the GUI context of the ExerciseViewer sub-application. It contains helper methods
@@ -15,14 +15,22 @@ import java.util.Optional;
  *
  * @author Stefan Saring
  */
-public interface EVContext {
+interface EVContext {
 
     /**
      * Returns the primary stage (main window) of the application.
-     *
-     * @return Stage
      */
-    Stage getPrimaryStage();
+    val primaryStage: Stage
+
+    /**
+     * Returns the provider of application text resources.
+     */
+    val resources: AppResources
+
+    /**
+     * Returns the format utils class for the current unit system.
+     */
+    val formatUtils: FormatUtils
 
     /**
      * Displays a modal message dialog of the passed type with the specified message title and message
@@ -34,8 +42,8 @@ public interface EVContext {
      * @param messageKey the resource key for the message text
      * @param arguments list of objects which needs to be inserted in the message text (optional)
      */
-    void showMessageDialog(Window parent, Alert.AlertType alertType, String titleKey,
-                                           String messageKey, Object... arguments);
+    fun showMessageDialog(parent: Window, alertType: Alert.AlertType, titleKey: String,
+                          messageKey: String, vararg arguments: Any)
 
     /**
      * Displays a modal confirmation dialog for the specified parameters. By default the dialog
@@ -46,10 +54,11 @@ public interface EVContext {
      * @param titleKey resource key for the dialog title
      * @param messageKey resource key for the dialog message
      * @param buttonTypes list of buttons to be displayed as confirmation options (optional)
-     * @return an Optional that contains the result (selected button type) of the displayed dialog.
+     * @return an Optional that contains the result (selected button type) of the displayed dialog (use of optional for
+     * Java compatibility)
      */
-    Optional<ButtonType> showConfirmationDialog(Window parent, final String titleKey, String messageKey,
-                                                ButtonType... buttonTypes);
+    fun showConfirmationDialog(parent: Window, titleKey: String, messageKey: String,
+                               vararg buttonTypes: ButtonType): Optional<ButtonType>
 
     /**
      * Displays a modal text input dialog for the specified parameters.
@@ -58,21 +67,8 @@ public interface EVContext {
      * @param titleKey resource key for the dialog title
      * @param messageKey resource key for the dialog message
      * @param initialValue initial text value to be displayed
-     * @return Optional containing the entered String (can be empty text) or Optional.empty() when the user has cancelled the dialog
+     * @return an Optional containing the entered String (can be empty text) or Optional.empty() when the user has
+     * cancelled the dialog (use of optional for Java compatibility)
      */
-    Optional<String> showTextInputDialog(Window parent, String titleKey, String messageKey, String initialValue);
-
-    /**
-     * Returns the provider of application text resources.
-     *
-     * @return AppResources
-     */
-    AppResources getResources();
-
-    /**
-     * Returns the format utils class for the current unit system.
-     *
-     * @return the current FormatUtils instance
-     */
-    FormatUtils getFormatUtils();
+    fun showTextInputDialog(parent: Window, titleKey: String, messageKey: String, initialValue: String): Optional<String>
 }
