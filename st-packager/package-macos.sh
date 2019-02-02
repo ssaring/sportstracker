e7c45eea4e1bd04e8d6ce095f26e41faf916d110
Stefan Saring <projects@saring.de>
2019-02-02 15:10:48 +0100

    Improved macOS packaging script (collecting files for packaging and cleanup).

--- st-packager/package-macos.sh
+++ st-packager/package-macos.sh
@@ -1,22 +1,33 @@
 #!/bin/sh
 
-JPACKAGER_HOME=./jdk.packager-osx
+ST_PROJECT_DIR=../sportstracker
+JPACKAGER_DIR=./jdk.packager-osx
 BUILD_DIR=./build
 PACKAGE_DIR=./package
 
+ST_VERSION=7.5.1-SNAPSHOT
+
+# cleanup
+rm -fr $BUILD_DIR
 rm -fr $PACKAGE_DIR
 
+# copy SportsTracker files and documentation
+mkdir $BUILD_DIR
+cp $ST_PROJECT_DIR/target/sportstracker-$ST_VERSION.jar $BUILD_DIR
+cp -R $ST_PROJECT_DIR/target/lib $BUILD_DIR
+cp -R $ST_PROJECT_DIR/docs $BUILD_DIR
+
 # use create-image or create-installer depending on the needed package type
-$JPACKAGER_HOME/jpackager create-image \
+$JPACKAGER_DIR/jpackager create-image \
     --verbose \
     --echo-mode \
     --input $BUILD_DIR \
-    --main-jar ./sportstracker-7.5.1-SNAPSHOT.jar \
+    --main-jar ./sportstracker-$ST_VERSION.jar \
     --class de.saring.sportstracker.STMain \
     --output $PACKAGE_DIR \
     --identifier de.saring.sportstracker \
     --name SportsTracker \
-    --version 7.5.1 \
+    --version $ST_VERSION \
     --vendor Saring.de \
     --copyright "(C) 2019 Stefan Saring" \
     --description "Application for tracking your sporting activities." \
@@ -26,3 +37,6 @@ $JPACKAGER_HOME/jpackager create-image \
     --module-path $JAVA_HOME/jmods \
     --add-modules java.base,java.desktop,java.logging,java.scripting,java.sql,java.xml,jdk.localedata,jdk.jsobject,jdk.unsupported \
     --strip-native-commands
+
+# delete temporary build directory
+rm -fr $BUILD_DIR
