004afa8889e8826805e2f7cb2aa4fba4252f8a65
Stefan Saring <projects@saring.de>
2019-03-07 21:44:03 +0100

    Ported all data test classes in st-util to Kotlin.

---
+++ st-util/src/test/kotlin/de/saring/util/data/IdDateObjectListTest.kt
@@ -0,0 +1,151 @@
+package de.saring.util.data
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.ArrayList
+
+/**
+ * Unit tests of class IdDateObjectList.
+ *
+ * @author Stefan Saring
+ */
+class IdDateObjectListTest {
+
+    /** The instance to test. */
+    private lateinit var list: IdDateObjectList<DateNameObject>
+
+    @BeforeEach
+    fun setUp() {
+        list = IdDateObjectList()
+        list.set(DateNameObject(1, LocalDateTime.of(2009, 2, 5, 21, 30, 0), "one"))
+        list.set(DateNameObject(2, LocalDateTime.of(2008, 12, 11, 20, 30, 0), "two"))
+        list.set(DateNameObject(3, LocalDateTime.of(2009, 2, 7, 11, 40, 0), "three"))
+
+        checkDateOrder()
+    }
+
+    /**
+     * Test of set(): must add a new object to list at position 2.
+     */
+    @Test
+    fun testSetAdd() {
+        list.set(DateNameObject(4, LocalDateTime.of(2008, 12, 25, 21, 30, 0), "four"))
+        assertEquals(list.size(), 4)
+        assertEquals("four", list.getAt(1).name)
+        checkDateOrder()
+    }
+
+    /**
+     * Test of set(): must replace the object in list at position 3 with the new
+     * one with the allready used ID.
+     */
+    @Test
+    fun testSetReplaceNew() {
+        assertEquals("three", list.getAt(2).name)
+        list.set(DateNameObject(3, LocalDateTime.of(2009, 2, 7, 11, 40, 0), "three-new"))
+        assertEquals(list.size(), 3)
+        assertEquals("three-new", list.getAt(2).name)
+        checkDateOrder()
+    }
+
+    /**
+     * Test of set(): must store the modified object in list from position 3 at
+     * the proper list position for the modified date.
+     */
+    @Test
+    fun testSetReplaceModified() {
+        val no3 = list.getAt(2)
+        assertEquals("three", no3.name)
+        no3.dateTime = LocalDateTime.of(2008, 7, 12, 12, 45, 0)
+        no3.name = "three-new"
+
+        list.set(no3)
+        assertEquals(list.size(), 3)
+        assertEquals("three-new", list.getAt(0).name)
+        checkDateOrder()
+    }
+
+    /**
+     * Test of method clearAndAddAll(). The previous list content must be removed, the
+     * list must contain only the new entries, sorted by date.
+     */
+    @Test
+    fun clearAndAddAll() {
+
+        val tempEntries = ArrayList<DateNameObject>()
+        tempEntries.add(DateNameObject(5, LocalDateTime.of(2009, 2, 5, 21, 30, 0), "five"))
+        tempEntries.add(DateNameObject(6, LocalDateTime.of(2009, 2, 1, 21, 30, 0), "six"))
+        list.clearAndAddAll(tempEntries)
+
+        assertEquals(2, list.size())
+        assertEquals("six", list.getAt(0).name)
+        assertEquals("five", list.getAt(1).name)
+    }
+
+    /**
+     * Test of getEntriesInDateRange(): must find all entries, the date range
+     * includes the earliest and latest entry.
+     */
+    @Test
+    fun testGetEntriesInDateRangeFindAll() {
+        val lFound = list.getEntriesInDateRange(
+                LocalDate.of(2008, 12, 11), LocalDate.of(2009, 2, 7))
+
+        assertEquals(3, lFound.size)
+        assertEquals("two", lFound[0].name)
+        assertEquals("one", lFound[1].name)
+        assertEquals("three", lFound[2].name)
+    }
+
+    /**
+     * Test of getEntriesInDateRange(): must find only one entries, the date
+     * range does not include the earliest and latest entry.
+     */
+    @Test
+    fun testGetEntriesInDateRangeFindOne() {
+        val lFound = list.getEntriesInDateRange(
+                LocalDate.of(2008, 12, 12), LocalDate.of(2009, 2, 6))
+
+        assertEquals(1, lFound.size)
+        assertEquals("one", lFound[0].name)
+    }
+
+    /**
+     * Test of getEntriesInDateRange(): must fail when the begin date ist after end date.
+     */
+    @Test
+    fun testGetEntriesInDateRangeInvalidRange() {
+        assertThrows(IllegalArgumentException::class.java) {
+            list.getEntriesInDateRange(
+                    LocalDate.of(2009, 12, 11),
+                    LocalDate.of(2009, 11, 11))
+        }
+    }
+
+    /**
+     * This helper method makes sure that all IdDateObjects in the list are in
+     * correct chronological order (the date of the previous exercise is never
+     * bigger then the date of the current exercise).
+     */
+    private fun checkDateOrder() {
+        var previousDate = LocalDateTime.of(1970, 1, 1, 0, 0, 0)
+
+        for (temp in list) {
+            assertTrue(previousDate.isBefore(temp.dateTime))
+            previousDate = temp.dateTime
+        }
+    }
+
+    /**
+     * Subclass of abstract class IdDateObject for testing.
+     */
+    internal class DateNameObject(id: Int, dateTime: LocalDateTime, var name: String) : IdDateObject(id) {
+
+        init {
+            this.dateTime = dateTime
+        }
+    }
+}
---
+++ st-util/src/test/kotlin/de/saring/util/data/IdObjectListTest.kt
@@ -0,0 +1,140 @@
+package de.saring.util.data
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.util.ArrayList
+
+/**
+ * Unit tests of class IdObjectList.
+ *
+ * @author Stefan Saring
+ */
+class IdObjectListTest {
+
+    /** The instance to test. */
+    private lateinit var list: IdObjectList<NameObject>
+
+    @BeforeEach
+    fun setUp() {
+
+        // create a new list with some content
+        list = IdObjectList()
+        list.set(NameObject(1, "one"))
+        list.set(NameObject(2, "two"))
+        list.set(NameObject(3, "three"))
+    }
+
+    /**
+     * Test of getByID method, of class IdObjectList.
+     */
+    @Test
+    fun getByID() {
+        assertNull(list.getByID(0))
+        assertNull(list.getByID(1000))
+        assertEquals("one", list.getByID(1)?.name)
+        assertEquals("three", list.getByID(3)?.name)
+    }
+
+    /**
+     * Test of getAt method, of class IdObjectList.
+     */
+    @Test
+    fun getAt() {
+        assertEquals("one", list.getAt(0).name)
+        assertEquals("three", list.getAt(2).name)
+
+        assertThrows(IndexOutOfBoundsException::class.java) { list.getAt(3) }
+    }
+
+    /**
+     * Test of indexOf method, of class IdObjectList.
+     */
+    @Test
+    fun indexOf() {
+        assertEquals(1, list.indexOf(NameObject(2, "two")))
+    }
+
+    /**
+     * Test of contains method, of class IdObjectList.
+     */
+    @Test
+    fun contains() {
+        assertTrue(list.contains(NameObject(2, "two")))
+        assertFalse(list.contains(NameObject(4, "four")))
+    }
+
+    /**
+     * Test of set method set() of class IdObjectList.
+     */
+    @Test
+    fun set() {
+
+        // this should add an element to list
+        list.set(NameObject(4, "four"))
+        assertEquals(4, list.size())
+        assertEquals("four", list.getAt(3).name)
+
+        // this should replace a list-element
+        list.set(NameObject(2, "zwei"))
+        assertEquals(4, list.size())
+        assertEquals("zwei", list.getByID(2)?.name)
+    }
+
+    /**
+     * Test of method clearAndAddAll(). The previous list content must be removed, the
+     * list must contain only the new entries.
+     */
+    @Test
+    fun clearAndAddAll() {
+
+        val tempEntries = ArrayList<NameObject>()
+        tempEntries.add(NameObject(5, "five"))
+        tempEntries.add(NameObject(6, "six"))
+        list.clearAndAddAll(tempEntries)
+
+        assertEquals(2, list.size())
+        assertEquals("five", list.getAt(0).name)
+        assertEquals("six", list.getAt(1).name)
+    }
+
+    /**
+     * Test of removeByID method, of class IdObjectList.
+     */
+    @Test
+    fun removeByID() {
+
+        // this delete must work
+        assertTrue(list.removeByID(3))
+        assertEquals(2, list.size())
+        assertNull(list.getByID(3))
+
+        // this delete can't work
+        assertFalse(list.removeByID(3))
+        assertEquals(2, list.size())
+    }
+
+    /**
+     * Test of getNewID method, of class IdObjectList.
+     */
+    @Test
+    fun getNewID() {
+
+        // ID's 1-3 in use => next needs to be 4
+        assertEquals(list.getNewId(), 4)
+
+        // add ID's 4 and 6 to list => next needs to be 5
+        list.set(NameObject(4, "four"))
+        list.set(NameObject(6, "six"))
+        assertEquals(5, list.getNewId())
+
+        // remove ID 2 from list => next needs to be 2
+        list.removeByID(2)
+        assertEquals(2, list.getNewId())
+    }
+
+    /**
+     * Subclass of abstract class IdObject for testing.
+     */
+    internal class NameObject(id: Int, val name: String) : IdObject(id)
+}
