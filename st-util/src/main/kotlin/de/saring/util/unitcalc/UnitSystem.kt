956a48cdf45094f2fdae4c1bb3cbc10a45d7d0eb
Stefan Saring <projects@saring.de>
2019-03-09 22:00:37 +0100

    Ported enums SpeedMode and UnitSystem to Kotlin.

---
+++ st-util/src/main/kotlin/de/saring/util/unitcalc/UnitSystem.kt
@@ -0,0 +1,14 @@
+package de.saring.util.unitcalc
+
+/**
+ * This is the list of possible unit systems.
+ *
+ * @author Stefan Saring
+ */
+enum class UnitSystem {
+
+    /** Metric unit system. Examples: kilometers for distance or celsius for temperature. */
+    METRIC,
+    /** English unit system. Examples: miles for distance and fahrenheit for temperature. */
+    ENGLISH
+}
