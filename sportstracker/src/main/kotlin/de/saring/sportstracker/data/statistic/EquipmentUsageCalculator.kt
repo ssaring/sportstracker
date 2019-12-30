b92801b7682c2f0366035cee97c71c70171ca397
Stefan Saring <projects@saring.de>
2019-12-30 13:15:11 +0100

    Improvements and documentation in EquipmentUsageCalculator.

--- sportstracker/src/main/kotlin/de/saring/sportstracker/data/statistic/EquipmentUsageCalculator.kt
+++ sportstracker/src/main/kotlin/de/saring/sportstracker/data/statistic/EquipmentUsageCalculator.kt
@@ -6,9 +6,21 @@ import de.saring.sportstracker.data.SportType
 import de.saring.sportstracker.data.SportTypeList
 import java.time.LocalDate
 
-// TODO document, integrate and test
+/**
+ * Calculator for the usage of equipment, grouped by sport types.
+ *
+ * @author Stefan Saring
+ */
 object EquipmentUsageCalculator {
 
+    /**
+     * Calculates the usage of equipment in all exercises. The usage will be calculated for all equipments defined
+     * in the passed sport types, also when it has not been used.
+     *
+     * @param exerciseList list of all Exercises
+     * @param sportTypeList list of all SportTypes
+     * @return map of equipment usages, grouped by sport types
+     */
     fun calculateEquipmentUsage(exerciseList: ExerciseList, sportTypeList: SportTypeList): EquipmentUsages {
 
         val equipmentUsages = createInitialEquipmentUsages(sportTypeList)
@@ -16,20 +28,18 @@ object EquipmentUsageCalculator {
         exerciseList.forEach { exercise ->
             exercise.equipment?.let { equipment ->
 
-                val exerciseDate = exercise.dateTime.toLocalDate()
-
-                // TODO without !! ?
-                val equipmentUsage: EquipmentUsage = equipmentUsages.usages[exercise.sportType]!!.usages[equipment]!!
+                val eqUsagesInSportType = equipmentUsages.sportTypeMap[exercise.sportType] ?: error("Not found for SportType!")
+                val equipmentUsage: EquipmentUsage = eqUsagesInSportType.equipmentMap[equipment] ?: error("Not found for Equipment!")
 
                 equipmentUsage.distance += exercise.distance
                 equipmentUsage.duration += exercise.duration
 
-                // TODO can this be done simpler?
-                equipmentUsage.firstUsage = if (equipmentUsage.firstUsage == null) exerciseDate else {
-                    if (exerciseDate.isBefore(equipmentUsage.firstUsage)) exerciseDate else equipmentUsage.firstUsage
+                val exerciseDate = exercise.dateTime.toLocalDate()
+                if (equipmentUsage.firstUsage == null || exerciseDate.isBefore(equipmentUsage.firstUsage)) {
+                    equipmentUsage.firstUsage = exerciseDate
                 }
-                equipmentUsage.lastUsage = if (equipmentUsage.lastUsage == null) exerciseDate else {
-                    if (exerciseDate.isAfter(equipmentUsage.lastUsage)) exerciseDate else equipmentUsage.lastUsage
+                if (equipmentUsage.lastUsage == null || exerciseDate.isAfter(equipmentUsage.lastUsage)) {
+                    equipmentUsage.lastUsage = exerciseDate
                 }
             }
         }
@@ -46,10 +56,29 @@ object EquipmentUsageCalculator {
     }
 }
 
-class EquipmentUsages(val usages: Map<SportType, EquipmentUsagesInSportType>)
+/**
+ * Container class for all equipment usages.
+ *
+ * @property sportTypeMap map of usages grouped by sport types
+ */
+class EquipmentUsages(val sportTypeMap: Map<SportType, EquipmentUsagesInSportType>)
 
-class EquipmentUsagesInSportType(val usages: Map<Equipment, EquipmentUsage>)
+/**
+ * Container class for the equipment usages in one single sport type.
+ *
+ * @property equipmentMap map of usages grouped by equipments
+ */
+class EquipmentUsagesInSportType(val equipmentMap: Map<Equipment, EquipmentUsage>)
 
+/**
+ * Container class for the usage of one single equipment.
+ *
+ * @property equipment the used equipment
+ * @property distance: total usage distance in kilometers
+ * @property duration total usage duration in seconds
+ * @property firstUsage first usage date of the equipment (null when unused)
+ * @property lastUsage last usage date of the equipment (null when unused)
+ */
 class EquipmentUsage(
         val equipment: Equipment,
         var distance: Double = 0.0,
